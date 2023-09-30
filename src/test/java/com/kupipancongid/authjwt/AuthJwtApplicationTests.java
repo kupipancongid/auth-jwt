36180454eb35913a62795031b118a597e348bf77
kupipancongid <idkupipancong@gmail.com>
2023-09-30 09:08:24 +0700

    spring boot initialization

---
+++ src/test/java/com/kupipancongid/authjwt/AuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kupipancongid.authjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
